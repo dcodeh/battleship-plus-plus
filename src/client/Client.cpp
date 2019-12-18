69b4a637405ffea511ac4282001249b3ce0a70e7
dcodeh <dcodyburrows@gmail.com>
2019-12-18 08:56:30 -0500

    Waist-deep in linker errors
    - Hooked up the Server and Client to the proxies and such
    - The linker is complaining about vtables and finding functions that
      I definitely implemented...

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -32,65 +32,55 @@ int main (int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    // TODO DCB remove this
-    printf("Connecting to %s port %s\n", ip, port);
-    View v();
-    ModelProxy mp();
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
 
-//    int status;
-//    struct addrinfo hints;
-//    struct addrinfo *servinfo;
-//
-//    memset(&hints, 0, sizeof(hints));
-//    hints.ai_family = AF_UNSPEC;        // USE IPV 4 or 6 I don't care
-//    hints.ai_socktype = SOCK_STREAM;    // Use TCP
-//
-//    status = getaddrinfo(ip, port, &hints, &servinfo);
-//    if (status != 0) {
-//        fprintf(stderr, "FATAL: failed to connect to server: %s\n",
-//                gai_strerror(status));
-//        return EXIT_FAILURE;
-//    }
-//
-//    // try to connect to the first valid interface
-//    int sockfd;
-//    struct addrinfo *p;
-//    for (p = servinfo; p != NULL; p = p -> ai_next) {
-//        if ((sockfd = socket(p -> ai_family, p -> ai_socktype, p -> ai_protocol)) == -1) {
-//            perror("socket call failed");
-//            continue;
-//        }
-//
-//        if (connect(sockfd, p -> ai_addr, p -> ai_addrlen) == -1) {
-//            close(sockfd);
-//            perror("connect failed");
-//            continue;
-//        }
-//
-//        break;
-//    }
-//
-//    if (p == NULL) {
-//        fprintf(stderr, "Failed to connect to server.\n");
-//        return EXIT_FAILURE;
-//    }
-//
-//    char s[INET6_ADDRSTRLEN];
-//    inet_ntop(p -> ai_family, get_in_addr((struct sockaddr *) p -> ai_addr), s, sizeof(s));
-//    printf("Connecting to BPP server %s\n", s);
-//
-//    freeaddrinfo(servinfo);
-//
-//    char buf[100];
-//    int numbytes;
-//    if ((numbytes = recv(sockfd, buf, 99, 0)) == -1) {
-//        perror("recv");
-//        return EXIT_FAILURE;
-//    }
-//
-//    buf[numbytes] = '\0';
-//    printf("received %s\n", buf);
-//
-//    close(sockfd);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;        // USE IPV 4 or 6 I don't care
+    hints.ai_socktype = SOCK_STREAM;    // Use TCP
+
+    status = getaddrinfo(ip, port, &hints, &servinfo);
+    if (status != 0) {
+        fprintf(stderr, "FATAL: failed to connect to server: %s\n",
+                gai_strerror(status));
+        return EXIT_FAILURE;
+    }
+
+    // try to connect to the first valid interface
+    int sockfd;
+    struct addrinfo *p;
+    for (p = servinfo; p != NULL; p = p -> ai_next) {
+        if ((sockfd = socket(p -> ai_family, p -> ai_socktype, p -> ai_protocol)) == -1) {
+            perror("socket call failed");
+            continue;
+        }
+
+        if (connect(sockfd, p -> ai_addr, p -> ai_addrlen) == -1) {
+            close(sockfd);
+            perror("connect failed");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "Failed to connect to server.\n");
+        return EXIT_FAILURE;
+    }
+
+    char s[INET6_ADDRSTRLEN];
+    inet_ntop(p -> ai_family, get_in_addr((struct sockaddr *) p -> ai_addr), s, sizeof(s));
+    printf("Connecting to BPP server %s\n", s);
+
+    freeaddrinfo(servinfo);
+    
+    View view();
+    ModelProxy proxy(sockfd);
+    view.set_listener(&proxy);
+    proxy.set_listener(&view);
+
+    close(sockfd);
     return EXIT_SUCCESS;
 }
--- src/serv/Server.cpp
+++ src/serv/Server.cpp
@@ -21,8 +21,8 @@
 #include <netdb.h>
 #include <unistd.h>
 
-#include "ModelListener.h"
 #include "Model.h"
+#include "ModelListener.h"
 #include "ViewListener.h"
 #include "ViewProxy.h"
 #include "Common.h"
@@ -43,104 +43,105 @@ int main (int argc, char ** argv) {
         return EXIT_FAILURE;
     }
 
-    Model m();
-    ViewProxy vp();
-    printf("Created a new model object...\n");
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
 
-    // TODO DCB remove this later...
-    printf("%s\n", port);
+    memset(&hints, 0, sizeof(hints));   // init to 0
+    hints.ai_family = AF_UNSPEC;        // Use whatever ip standard
+    hints.ai_socktype = SOCK_STREAM;    // use TCP
+    hints.ai_flags = AI_PASSIVE;        // determine IP addr automagically
 
-//    int status;
-//    struct addrinfo hints;
-//    struct addrinfo *servinfo;
-//
-//    memset(&hints, 0, sizeof(hints));   // init to 0
-//    hints.ai_family = AF_UNSPEC;        // Use whatever ip standard
-//    hints.ai_socktype = SOCK_STREAM;    // use TCP
-//    hints.ai_flags = AI_PASSIVE;        // determine IP addr automagically
-//
-//    status = getaddrinfo(NULL, port, &hints, &servinfo);
-//    if (status != 0) {
-//        fprintf(stderr, "FATAL: failed to get host address info: %s\n",
-//                gai_strerror(status));
-//        return EXIT_FAILURE;
-//    }
-//
-//    // find the first result we can bind to
-//    int sockfd; // socket file descroptor
-//    int new_fd; // new connections
-//    int yes = 1;
-//    struct addrinfo *p;
-//    for (p = servinfo; p != NULL; p = p->ai_next) {
-//        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-//            perror("socket");
-//            continue;
-//        }
-//
-//        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-//            perror("setsockopt");
-//            continue;
-//        }
-//
-//        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-//            close(sockfd);
-//            perror("bind");
-//            continue;
-//        }
-//
-//        break;
-//    }
-//
-//    freeaddrinfo(servinfo);
-//
-//    if (p == NULL) {
-//        fprintf(stderr, "FATAL: unable to bind server socket.\n");
-//        return EXIT_FAILURE;
-//    }
-//
-//    if (listen(sockfd, BACKLOG) == -1) {
-//        perror("listen");
-//        return EXIT_FAILURE;
-//    }
-//
-//    struct sigaction sa;
-//    struct sockaddr_storage their_addr;
-//    sa.sa_handler = sigchld_handler; // reap all dead processes
-//    sigemptyset(&sa.sa_mask);
-//    sa.sa_flags = SA_RESTART;
-//    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-//        perror("sigaction");
-//        return EXIT_FAILURE;
-//    }
-//
-//    printf("Waiting for connections\n");
-//
-//    // accept all new connections
-//    socklen_t sin_size;
-//    char s[INET6_ADDRSTRLEN];
-//    while (true) {
-//        sin_size = sizeof(their_addr);
-//        new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
-//        if (new_fd == -1) {
-//            perror("accept");
-//            continue;
-//        }
-//
-//        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *) &their_addr), s, sizeof(s));
-//        printf("New connection from %s\n", s);
-//
-//        if (!fork()) {
-//            // only run by the child process
-//            close(sockfd);
-//            if(send(new_fd, "HELLOW WORLD!", 13, 0) == -1) {
-//                perror("send");
-//            }
-//            close(new_fd);
-//            // child process exits
-//            return EXIT_SUCCESS;
-//        }
-//        close(new_fd); // paren't doesn't use this
-//    }
+    status = getaddrinfo(NULL, port, &hints, &servinfo);
+    if (status != 0) {
+        fprintf(stderr, "FATAL: failed to get host address info: %s\n",
+                gai_strerror(status));
+        return EXIT_FAILURE;
+    }
+
+    // find the first result we can bind to
+    int sockfd; // socket file descroptor
+    int new_fd; // new connections
+    int yes = 1;
+    struct addrinfo *p;
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("setsockopt");
+            continue;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo);
+
+    if (p == NULL) {
+        fprintf(stderr, "FATAL: unable to bind server socket.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        return EXIT_FAILURE;
+    }
+
+    struct sigaction sa;
+    struct sockaddr_storage their_addr;
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        return EXIT_FAILURE;
+    }
+
+    printf("Waiting for connections\n");
+
+    // accept all new connections
+    socklen_t sin_size;
+    char s[INET6_ADDRSTRLEN];
+    Model *model = new Model(); // TODO DCB use new?
+
+    // TODO DCB add check for model done listening for new connections
+    while (true) {
+        sin_size = sizeof(their_addr);
+        new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *) &their_addr), s, sizeof(s));
+        printf("Accepting new connection from %s\n", s);
+
+        // TODO DCB I don't think this is necessary: remove child handling stuff
+        // if (!fork()) {
+        //     // only run by the child process
+        //     close(sockfd);
+        //     if(send(new_fd, "HELLOW WORLD!", 13, 0) == -1) {
+        //         perror("send");
+        //     }
+        //     close(new_fd);
+        //     // child process exits
+        //     return EXIT_SUCCESS;
+        // }
+        // close(new_fd); // paren't doesn't use this
+
+        ModelListener *proxy = new ViewProxy(new_fd);
+        model->register_listener(proxy);
+        ((ViewProxy *) proxy)->set_listener(model);
+    }
 
     return EXIT_SUCCESS;
 }
