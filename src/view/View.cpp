8bbcb103ec1f5d8fb7369903800300ab7bef52ee
Cody Burrows <dcodyburrows@gmail.com>
2020-01-11 16:05:30 -0500

    Oh the memory leaks you'll fix later...
    - Saw a lot of potentially embarassing things that will surely need to be fixed
      in the future.
    - Finished adding documentation, and adding TODOs

--- src/view/View.cpp
+++ src/view/View.cpp
@@ -10,31 +10,37 @@
 #include <stdio.h>
 #include <mutex>
 
+/** Inform the view of the server's version */
 void View::version(char *ver_string) {
     std::unique_lock<std::mutex> lock (_mutex);
     printf("version %s\n", ver_string);
 }
 
+/** Inform the view that it should quit. */
 void View::quit() {
     std::unique_lock<std::mutex> lock (_mutex);
     printf("die\n");
     m_alive = false;
 }
 
+/** Pass a note to the view. */
 void View::info(char *msg) {
     std::unique_lock<std::mutex> lock (_mutex);
     printf("info %s\n", msg);
 }
 
+/** Give the view a slap on the wrist. */
 void View::err(char *msg) {
     std::unique_lock<std::mutex> lock (_mutex);
     printf("err %s\n", msg);
 }
 
+/** Hook the view up to a model object */
 void View::set_listener(ViewListener *m) {
     m_listener = m;
 }
 
+/** Poll whether or not the view is still listening for messages */
 bool View::alive() {
     return m_alive;
 }
--- src/view/View.h
+++ src/view/View.h
@@ -36,16 +36,45 @@ class View : public ModelListener {
         ~View() {
         }
 
+        /**
+          * Tell the view the server's version.
+          *
+          * @param ver_string The version string, such as 10.15.3
+          */
         void version(char *ver_string);
 
+        /**
+          * Tell the view the server wants it to quit.
+          */
         void quit();
 
+        /**
+          * Deliver an information message from the server
+          *
+          * @param msg The message
+          */
         void info(char *msg);
 
+        /**
+          * Deliver an error message from the server, likely indicating that the
+          * view did something wrong. Punishment not yet implemented.
+          *
+          * @param msg What went wrong
+          */
         void err(char *msg);
 
+        /**
+          * Hook up the model (or listener) that listens to this object.
+          *
+          * @param m The Model.
+          */
         void set_listener(ViewListener *m);
 
+        /**
+          * Poll whether or not the view is still listening for messages.
+          *
+          * @return true if the view is still listening. False otherwise.
+          */
         bool alive();
 };
 
